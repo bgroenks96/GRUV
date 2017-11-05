a1437edf438e1ecf6225a3f2caed63d23e09d39b
Brian Groenke <bgroe8@gmail.com>
2017-11-04 20:53:47 -0400

    Revert 'stateful' setting from LSTM layers.

--- nn_utils/network_utils.py
+++ nn_utils/network_utils.py
@@ -8,7 +8,7 @@ def create_lstm_network(num_frequency_dimensions, num_hidden_dimensions, num_rec
     model.add(TimeDistributed(Dense(num_hidden_dimensions), input_shape=(None, num_frequency_dimensions)))
     model.add(GaussianDropout(0.2))
     for cur_unit in xrange(num_recurrent_units):
-        model.add(LSTM(units=num_hidden_dimensions, return_sequences=True, stateful=True))
+        model.add(LSTM(units=num_hidden_dimensions, return_sequences=True))
     #This layer converts hidden space back to frequency space
     model.add(TimeDistributed(Dense(num_frequency_dimensions)))
     model.compile(loss='mean_squared_error', optimizer='rmsprop')
