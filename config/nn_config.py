d5d5df6ab5a91b2d098e8ba26b967d6d2c30f6ec
Brian Groenke <groenke.5@buckeyemail.osu.edu>
2017-11-25 20:13:17 -0500

    Fix decoder network.

--- config/nn_config.py
+++ config/nn_config.py
@@ -3,12 +3,12 @@ from keras import optimizers
 def get_neural_net_configuration():
 	nn_params = {}
 	nn_params['sampling_frequency'] = 44100
-	nn_params['generator_hidden_dims'] = 256
+	nn_params['generator_hidden_dims'] = 512
 	nn_params['generator_dropout'] = 0.2
 	nn_params['generator_optimizer'] = 'adam'
 	nn_params['decoder_dropout'] = 0.2
 	nn_params['decoder_optimizer'] = 'adam'
-	nn_params['decoder_hidden_dims'] = 256
+	nn_params['decoder_hidden_dims'] = 128
 	nn_params['combined_optimizer'] = 'adamax'
 	#The weights filename for saving/loading trained models
 	nn_params['model_basename'] = './NuGruvModelWeights'
--- nn_utils/network_utils.py
+++ nn_utils/network_utils.py
@@ -24,7 +24,7 @@ def create_gru_network(num_frequency_dimensions, num_hidden_dimensions):
     model.add(TimeDistributed(Dense(num_frequency_dimensions)))
     model.compile(loss='mean_squared_error', optimizer='rmsprop')
     return model
-    
+
 def create_noise_network(num_frequency_dimensions, num_hidden_dimensions):
     model = Sequential()
     model.add(TimeDistributed(Lambda(lambda x: np.random.choice((0,1), p=[0.1,0.9])*np.random.normal(1,0.3)*x*0.5), input_shape=(None, num_frequency_dimensions)))
@@ -49,9 +49,9 @@ def create_decoder_network(num_frequency_dimensions, num_timesteps, config):
     dropout = GaussianDropout(['decoder_dropout'])
     inputs = Input(shape=(num_timesteps, num_frequency_dimensions))
     conv_in = Conv1D(num_hidden_dimensions, kernel_size=2, activation='tanh', padding='causal')(inputs)
-    dense_h0 = Dense(num_hidden_dimensions / 2, activation='tanh')(Flatten()(dropout(conv_in)))
-    dense_h1 = Dense(num_hidden_dimensions / 4, activation='tanh')(dropout(dense_h1))
-    dense_out = Dense(1, activation='sigmoid')(dropout(dense_h0))
+    dense_h0 = TimeDistributed(Dense(num_hidden_dimensions, activation='tanh'))(conv_in)
+    lstm = LSTM(num_hidden_dimensions, return_sequences=False, activation='tanh')(dropout(dense_h0))
+    dense_out = Dense(1, activation='sigmoid')(dropout(lstm))
     decoder = Model(inputs=inputs, outputs=dense_out)
     decoder.compile(loss='binary_crossentropy', optimizer=config['decoder_optimizer'], metrics=['accuracy'])
     return decoder
@@ -75,11 +75,11 @@ class GAN:
         self.generator = generator
         self.decoder = decoder
         self.model = model
-    
+
     # Fit the generator network against the given training data
     def fit_generator(self, X_train, y_train, batch_size=None, epochs=10, shuffle=False, verbose=1, callbacks=None, validation_data=None):
         return self.generator.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, shuffle=shuffle, verbose=verbose, callbacks=callbacks, validation_data=validation_data)
-    
+
     # Fit the decoder network against the given real and fake X examples. An example output of '1' will be generated for each real example, and '0' for each fake one.
     def fit_decoder(self, X_real, X_fake, batch_size=None, epochs=10, shuffle=False, verbose=1, callbacks=None, validation_data=[]):
         num_real = X_real.shape[0]
@@ -94,7 +94,7 @@ class GAN:
             y_val = np.concatenate((np.ones((val_real.shape[0], 1)), np.zeros((val_fake.shape[0], 1))), axis=0)
             val_data = (X_val, y_val)
         return self.decoder.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, shuffle=shuffle, verbose=verbose, callbacks=callbacks, validation_data=val_data)
-    
+
     def fit(self, X_train, batch_size=None, epochs=10, shuffle=False, verbose=1, callbacks=None, validation_x=[]):
         num_examples = X_train.shape[0]
         num_timesteps = X_train.shape[1]
@@ -108,7 +108,7 @@ class GAN:
         hist = self.model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, shuffle=shuffle, verbose=verbose, callbacks=callbacks, validation_data=val_data)
         self.decoder.trainable = True
         return hist
-    
+
     def summary(self):
         print('==== Generator ====')
         self.generator.summary()
@@ -118,4 +118,3 @@ class GAN:
         self.decoder.trainable = False
         self.model.summary()
         self.decoder.trainable = True
-        
