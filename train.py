af8dd7365859e063dea37a3634f1b8da802cbff0
Brian Groenke <groenke.5@buckeyemail.osu.edu>
2017-11-30 21:28:10 -0500

    Fix train.py error

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
 input_file = config['dataset_directory'] + args.dataset_name + '/' + args.dataset_name
 cur_iter = args.start_iter
-model_basename = config['model_basename'] + args.run
+model_basename = config['model_basename'] + str(args.run)
 model_filename = '{0}_{2}'.format(model_basename, str(cur_iter))
 skip_validation = args.skip_validation
 
