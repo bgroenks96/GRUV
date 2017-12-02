b415546e3679ebc87da7d2415ee27dbfa0625008
Brian Groenke <bgroe8@gmail.com>
2017-12-01 23:37:59 -0500

    Rework GAN decoder network.

--- config/nn_config.py
+++ config/nn_config.py
@@ -32,11 +32,11 @@ def get_deconv_gan_configuration():
     nn_params = {}
     nn_params['sampling_frequency'] = 44100
     nn_params['generator_hidden_dims'] = 512
-    nn_params['generator_dropout'] = 0.2
+    nn_params['generator_dropout'] = 0.1
     nn_params['generator_optimizer'] = 'adam'
-    nn_params['decoder_dropout'] = 0.3
+    nn_params['decoder_dropout'] = 0.4
     nn_params['decoder_optimizer'] = optimizers.SGD(0.01, momentum=0.1)
-    nn_params['decoder_hidden_dims'] = 64
+    nn_params['decoder_hidden_dims'] = 32
     nn_params['combined_optimizer'] = optimizers.Adadelta(0.05)
     #The weights filename for saving/loading trained models
     nn_params['model_basename'] = './NuGruvModelWeights'
